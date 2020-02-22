2f9bb8dea5a081f220cb513de114330172a178fe
hquanitute <hquanitue@gmail.com>
2020-02-23 01:22:42 +0700

    init Navnar

--- src/components/LearnPage/index.js
+++ src/components/LearnPage/index.js
@@ -7,6 +7,14 @@ import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-min-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/theme-github";
 
+import {
+    ReflexContainer,
+    ReflexSplitter,
+    ReflexElement
+  } from 'react-reflex'
+   
+  import 'react-reflex/styles.css'
+
 const { Header, Footer, Sider, Content } = Layout;
 
 function onChange(newValue) {
@@ -18,30 +26,38 @@ function LearnPage(props) {
         <div>
             <Layout id="layout-main">
                 <Sider>Muc Luc</Sider>
-                <Layout>
-                    <Header>Tiêu đề</Header>
-                    <Content>Nội dung</Content>
-                    <Footer>Bye</Footer>
-                </Layout>
-                <Layout id="layout-code">
-                    <AceEditor
-                        mode="javascript"
-                        theme="github"
-                        name="aceeditorContainer"
-                        onChange={onChange}
-                        fontSize={16}
-                        showPrintMargin={true}
-                        showGutter={true}
-                        highlightActiveLine={true}
-                        value={`hello`}
-                        setOptions={{
-                            enableBasicAutocompletion: true,
-                            enableLiveAutocompletion: true,
-                            showLineNumbers: true,
-                            tabSize: 4,
-                        }} />
-                    <Content>Test pass or fail</Content>
-                </Layout>
+                <ReflexContainer orientation="vertical">
+                    <ReflexElement className="left-pane">
+                        <Layout>
+                            <Header>Tiêu đề</Header>
+                            <Content>Nội dung</Content>
+                            <Footer>Bye</Footer>
+                        </Layout>
+                    </ReflexElement>
+                    <ReflexSplitter />
+
+                    <ReflexElement className="right-pane">
+                        <Layout id="layout-code">
+                            <AceEditor
+                                mode="javascript"
+                                theme="github"
+                                name="aceeditorContainer"
+                                onChange={onChange}
+                                fontSize={16}
+                                showPrintMargin={true}
+                                showGutter={true}
+                                highlightActiveLine={true}
+                                value={`hello`}
+                                setOptions={{
+                                    enableBasicAutocompletion: true,
+                                    enableLiveAutocompletion: true,
+                                    showLineNumbers: true,
+                                    tabSize: 4,
+                                }} />
+                            <Content id="testInfo">Test pass or fail</Content>
+                        </Layout>
+                    </ReflexElement>
+                </ReflexContainer>
             </Layout>
         </div>
     );
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import LearnPage from '../LearnPage';
+import NavBar from '../NavBar';
 
 function Main(props) {
     return (
         <div>
+            <NavBar/>
             <LearnPage/>
         </div>
     );
