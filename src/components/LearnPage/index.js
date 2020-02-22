a980b1a27f9e3d028a7d13e81eac06e41e01e940
hquanitute <hquanitue@gmail.com>
2020-02-22 19:39:11 +0700

    Init project

---
+++ src/components/LearnPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LearnPage(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default LearnPage;
---
+++ src/components/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LearnPage from '../LearnPage';
+
+function Main(props) {
+    return (
+        <div>
+            <LearnPage/>
+        </div>
+    );
+}
+
+export default Main;
