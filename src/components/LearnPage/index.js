1fb0093b6f3dd594a119efdc3fc3b12b0f4756ee
hquanitute <hquanitue@gmail.com>
2020-02-22 20:34:21 +0700

    prepare component in Learn page

--- src/components/LearnPage/index.js
+++ src/components/LearnPage/index.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { Layout } from 'antd';
+import '../../style/css/learn.css';
+
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-min-noconflict/ext-language_tools";
+import "ace-builds/src-noconflict/theme-github";
+
+const { Header, Footer, Sider, Content } = Layout;
+
+function onChange(newValue) {
+    console.log("change", newValue);
+}
 
 function LearnPage(props) {
     return (
         <div>
-            
+            <Layout id="layout-main">
+                <Sider>Muc Luc</Sider>
+                <Layout>
+                    <Header>Tiêu đề</Header>
+                    <Content>Nội dung</Content>
+                    <Footer>Bye</Footer>
+                </Layout>
+                <Layout id="layout-code">
+                    <AceEditor
+                        mode="javascript"
+                        theme="github"
+                        name="aceeditorContainer"
+                        onChange={onChange}
+                        fontSize={16}
+                        showPrintMargin={true}
+                        showGutter={true}
+                        highlightActiveLine={true}
+                        value={`hello`}
+                        setOptions={{
+                            enableBasicAutocompletion: true,
+                            enableLiveAutocompletion: true,
+                            showLineNumbers: true,
+                            tabSize: 4,
+                        }} />
+                    <Content>Test pass or fail</Content>
+                </Layout>
+            </Layout>
         </div>
     );
 }
