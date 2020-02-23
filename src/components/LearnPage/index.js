608774419804dd7aa781b98d48269dba6e824d91
hquanitute <hquanitue@gmail.com>
2020-02-23 23:11:26 +0700

    display code from md to editor

--- src/components/LearnPage/index.js
+++ src/components/LearnPage/index.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { Layout } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Layout, Row } from 'antd';
 import '../../style/css/learn.css';
 
 import AceEditor from "react-ace";
-import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-min-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-terminal";
 
 import {
     ReflexContainer,
     ReflexSplitter,
     ReflexElement
-  } from 'react-reflex'
-   
-  import 'react-reflex/styles.css'
+} from 'react-reflex'
+
+import 'react-reflex/styles.css'
+
+import parseMD from 'parse-md'
+import marked from 'marked'
+
+import { useParams, useRouteMatch, Link } from 'react-router-dom';
+
+const ReactMarkdown = require('react-markdown')
 
 const { Header, Footer, Sider, Content } = Layout;
 
@@ -21,17 +29,83 @@ function onChange(newValue) {
     console.log("change", newValue);
 }
 
+
+
 function LearnPage(props) {
+    function chooseLesson(e) {
+        console.log(e)
+        setLessonName(e.target.key)
+    }
+    let { path, url } = useRouteMatch();
+
+    const [lessonName, setLessonName] = useState("");
+    const [challenge, setChallenge] = useState({ challengeSeed: "" });
+
+    let { topicId } = useParams();
+    const input = `<section id='instructions'>
+    Write chained <code>if</code>/<code>else if</code> statements to fulfill the following conditions: <br/>
+    <code>num &lt;   5</code> - return "Tiny"<br><code>num &lt;  10</code> - return "Small"<br><code>num &lt; 15</code> - return "Medium"<br><code>num &lt; 20</code> - return "Large"<br><code>num >= 20</code>  - return "Huge"
+    </section>`
+
+
+    useEffect(() => {
+        const fileContents = require("../../challenge/test.md");
+        fetch(fileContents)
+            .then(response => {
+                return response.text()
+            })
+            .then(text => {
+                // this.setState({
+                //     markdown: marked(text)
+                // })
+                const { metadata, content } = parseMD(text)
+                let contentRemovedKey = content.replace("## Description", "##").replace("## Instructions", "##").replace("## Tests", "##").replace("## Challenge Seed", "##").replace("## Solution", "##");
+                let arrStringContent = contentRemovedKey.split("##");
+                console.log(arrStringContent)
+                setChallenge({
+                    ...challenge,
+                    id: metadata.id,
+                    title: metadata.title,
+                    challengeType: metadata.challengeType,
+                    videoUrl: metadata.videoUrl,
+                    forumTopicId: metadata.forumTopicId,
+                    description: arrStringContent[1],
+                    instructions: arrStringContent[2],
+                    tests: arrStringContent[3],
+                    challengeSeed: (arrStringContent[4]),
+                    solution: arrStringContent[5]
+                });
+            })
+
+    }, [topicId])
+
+    useEffect(() => {
+        console.log(challenge.challengeSeed)
+    }, [challenge])
+
+    useEffect(() => {
+        console.log(lessonName);
+    }, [lessonName])
     return (
         <div>
             <Layout id="layout-main">
-                <Sider>Muc Luc</Sider>
+                <Sider>
+                    <Row>
+                        Lesson
+                    </Row>
+                    <Link key="1" to={`${url}/rendering`}>
+                        <button onClick={() => setLessonName("abc")}>
+                            Rendering with React
+                        </button>
+                    </Link>
+                </Sider>
                 <ReflexContainer orientation="vertical">
                     <ReflexElement className="left-pane">
-                        <Layout>
-                            <Header>Tiêu đề</Header>
-                            <Content>Nội dung</Content>
-                            <Footer>Bye</Footer>
+                        <Layout id="layout-content">
+                            <Header id="title">{challenge.title}</Header>
+                            <Content>
+                                <ReactMarkdown source={challenge.description} escapeHtml={false} />
+                            </Content>
                         </Layout>
                     </ReflexElement>
                     <ReflexSplitter />
@@ -40,14 +114,14 @@ function LearnPage(props) {
                         <Layout id="layout-code">
                             <AceEditor
                                 mode="javascript"
-                                theme="github"
+                                theme="terminal"
                                 name="aceeditorContainer"
                                 onChange={onChange}
                                 fontSize={16}
                                 showPrintMargin={true}
                                 showGutter={true}
                                 highlightActiveLine={true}
-                                value={`hello`}
+                                value={challenge.challengeSeed}
                                 setOptions={{
                                     enableBasicAutocompletion: true,
                                     enableLiveAutocompletion: true,
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,14 +1,57 @@
 import React from 'react';
 import LearnPage from '../LearnPage';
-import NavBar from '../NavBar';
+import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Row, Col } from 'antd';
+import { Input } from 'antd';
+import '../../style/css/navbar.css';
+const { Search } = Input;
 
 function Main(props) {
     return (
         <div>
-            <NavBar/>
-            <LearnPage/>
+            <Router>
+                <div>
+                    <nav>
+                        <Row>
+                            <Col span={6}>
+                                <Search id="search-input" placeholder="input search text" onSearch={value => console.log(value)} enterButton />
+                            </Col>
+                            {/* Offset: grid=24 grid-6:for search. offset = ((24/2-6)-(span))/2= 5 */}
+                            <Col span={2} offset={5}> 
+                                <Link to="/">
+                                    LearnCodeClient
+                                </Link>
+                            </Col>
+                            <Col span={1} offset={7}>
+                                <Link className="rightNav" to="/new">/new</Link>
+                            </Col>
+                            <Col span={1} offset={0}>
+                                <Link className="rightNav" to="/forum">/forum</Link>
+                            </Col>
+                            <Col span={1} offset={0}>
+                                <Link className="rightNav" to="/learn">/learn</Link>
+                            </Col>
+                        </Row>
+                    </nav>
+                    <Switch>
+                        <Route exat path="/learn">
+                            <LearnPage />
+                        </Route>
+                        {/* <Route path="/users">
+                            <Users />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route> */}
+                    </Switch>
+                </div>
+            </Router>
         </div>
     );
+}
+
+function Home() {
+    return <h2>Home</h2>;
 }
 
 export default Main;
