541e99bdb755b2416cba09f457391ca8a82e309a
hquanitute <hquanitute@gmail.com>
2020-07-26 20:14:40 +0700

    Add java

--- src/components/LearnPage/challenge.js
+++ src/components/LearnPage/challenge.js
@@ -101,14 +101,59 @@ function Challenge(props) {
         element.click();
         setModalVisible(false)
     };
+
+    const similarity = (s1, s2) => {
+        var longer = s1;
+        var shorter = s2;
+        if (s1.length < s2.length) {
+          longer = s2;
+          shorter = s1;
+        }
+        var longerLength = longer.length;
+        if (longerLength == 0) {
+          return 1.0;
+        }
+        return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+      }
+    
+    const editDistance = (s1, s2) => {
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+      
+        var costs = new Array();
+        for (var i = 0; i <= s1.length; i++) {
+          var lastValue = i;
+          for (var j = 0; j <= s2.length; j++) {
+            if (i == 0)
+              costs[j] = j;
+            else {
+              if (j > 0) {
+                var newValue = costs[j - 1];
+                if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                  newValue = Math.min(Math.min(newValue, lastValue),
+                    costs[j]) + 1;
+                costs[j - 1] = lastValue;
+                lastValue = newValue;
+              }
+            }
+          }
+          if (i > 0)
+            costs[s2.length] = lastValue;
+        }
+        return costs[s2.length];
+    }
+
     const submitCode = () => {
         let language = "";
         let codeSubmit = "";
+        let testString = "";
         if (props.challengeSelected.challengeType > -1 && props.challengeSelected.challengeType < 100) {
             language = "NodejsTest"
-            codeSubmit = props.challengeSelected.tests + " " + code
+            codeSubmit = "const codeSubmit = ` "+ code.replace(/\n+/g, ' ') + " ` ; " +props.challengeSelected.tests + " " + code
         } else if (props.challengeSelected.challengeType >= 100 && props.challengeSelected.challengeType < 200) {
-            language = "Java"
+            language = "JavaTest";
+            testString = props.challengeSelected.tests;
+            codeSubmit = code
         } else if (props.challengeSelected.challengeType >= 200 && props.challengeSelected.challengeType < 300) {
             language = "Python2";
             codeSubmit = code
@@ -120,15 +165,13 @@ function Challenge(props) {
             },
             "language": language,
             "test": {
-                "code": "string"
+                "code": testString
             }
         }
         trackPromise(callApiAsPromise("post", process.env.REACT_APP_COMPILE_SERVER + "code", null, JSON.stringify(data)).then((response) => {
-            if (response.data.errorMessage.errorComplieMessage == null && response.data.successMessage.successComplieMessage === props.challengeSelected.runResult) {
-                console.log(response.data.successMessage.successComplieMessage);
-                console.log(props.challengeSelected.runResult);
-                
+            if (response.data.errorMessage.errorComplieMessage == null && similarity(response.data.successMessage.successComplieMessage , props.challengeSelected.runResult) > 0.975 ) {                
                 setTestInfo(response.data.successMessage.successComplieMessage)
+                console.log(similarity(response.data.successMessage.successComplieMessage , props.challengeSelected.runResult));
                 if (props.userInfo._id){
                     const listChallengeIdPassed = props.userInfo.listChallengeIdPassed;                    
                     if (!listChallengeIdPassed.includes(challengeId)){
