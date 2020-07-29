b6b724134774fcc92e83316a9caac40bd051073c
hquanitute <hquanitute@gmail.com>
2020-07-29 21:49:34 +0700

    Delete statement

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,7 +12,7 @@ function Home(props) {
     return (
         <div className ='topic-bg min-h-screen'>
             <h1 className="pt-4 mb-6 font-bold">Welcome to Learn Code</h1>
-            <h2 className="mt-3 mb-10 font-normal">Learn Code on your bed</h2>
+            <h2 className="mt-3 mb-10 font-normal">Learn Code at Home</h2>
             <h2 className="mt-3 mb-10 font-normal">Discuss Technical</h2>
             <h3 className="mt-3 mb-10 font-normal">Dissertation of 2-member team - Quan and Dat, which will help you to improve coding skills</h3>
             {props.userInfo._id ? (<h2 className="my-4">Let's do it !!!</h2>) : (<h2 className="my-4">
